56df353b84bbb1223674c4a8f8d16594a61ce99a
Joao Paulo Pinheiro <joaopinheiro.ti@gmail.com>
2020-08-06 13:22:02 -0400

    Counter

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
 </template>
 
 <style>
+body {
+  background-color: azure;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Counter
+      :year="2020"
+      :month="7"
+      :date="5"
+      :hour="18"
+      :minute="27"
+      :second="10"
+      :millisecond="10"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Counter,
   },
 }
 </script>
+
+<style lang="scss" scoped></style>
